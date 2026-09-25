fc2bc2a066190200b53dd8a7024f287dfa8625c8
otundoMjoseph <otundomainjoseph@gmail.com>
2026-09-25 05:22:18 +0300

    added a new animation

--- Motion-Basics/src/App.tsx
+++ Motion-Basics/src/App.tsx
@@ -3,8 +3,13 @@ import { motion } from 'framer-motion'
 const App = () => {
   return (
     <motion.div className= "box"
-    
-    animate={{x: 100}}
+    initial={{x:0}}
+      animate={{
+        x: 100, rotate: [0, 270, 260, 200, 250, 250, 0, 270, 200, 0],
+      borderRadius: ["20%", "20%", "50%", "50%", "20%"]
+     }}
+    transition={{ duration: 20, repeat: Infinity }}  
+      
     />
   )
 }
